a2596d4fd6184ab8b17b1773d83fcd988210fa32
Fyre <fyre.homelab+github@gmail.com>
2022-05-18 11:17:17 -0500

    Implement db backups.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import time
 import mysql.connector
 from mysql.connector import Error
+
+BACKUP_PATH = './backup/'
 
 
 def print_banner():
@@ -24,12 +27,26 @@ def print_menu():
     while True:
         print("Select one of the following options: ")
         print("  1. M0 (1.12.1) to M1 (2.4.3)")
+        print("  2. M1 (2.4.3) to M2 (3.3.5a)")
+        print("  3. M2 (3.3.5a) to M3 (4.3.4)")
         print("  q. Quit")
         print("\n")
         choice = input("Enter choice: ")
         if choice == "1":
             print_banner()
             backup_db(0)
+            migrate_db(0)
+            break
+        elif choice == "2":
+            print_banner()
+            backup_db(1)
+            migrate_db(1)
+            break
+        elif choice == "3":
+            print_banner()
+            backup_db(2)
+            migrate_db(2)
+            break
         elif choice == "q":
             print("Bye!")
             break
@@ -40,11 +57,13 @@ def print_menu():
 def backup_db(version):
     mysql_user = input("Enter MySQL username [default: mangos]: ") or "mangos"
     mysql_pass = input("Enter MySQL password [default: mangos]: ") or "mangos"
-    mysql_host = input("Enter MySQL host [default: localhost]: ") or "localhost"
+    mysql_host = input("Enter MySQL host [default: 127.0.0.1]: ") or "127.0.0.1"
     mysql_port = int(input("Enter MySQL port [default: 3306]: ") or "3306")
-    db_mangosd_name = input("Enter mangosd/world database name [default: mangosd0]: ") or "mangosd0"
-    db_characters_name = input("Enter characters database name [default: characters0]: ") or "characters0"
+    db_mangosd_name = input(f"Enter mangosd/world database name [default: mangos{version}]: ") or f"mangos{version}"
+    db_characters_name = input(f"Enter characters database name [default: character{version}]: ") or f"character{version}"
 
+    # Test database connections before backing up.
+    success = 0
     for db_x in [db_mangosd_name, db_characters_name]:
         conn = None
         try:
@@ -52,27 +71,45 @@ def backup_db(version):
             
             if conn.is_connected():
                 print(f"Connected to database ({db_x})!")
+                success+=1
         except:
             print(f"Error connecting to database ({db_x})!")
         finally:
             if conn is not None and conn.is_connected():
                 conn.close()
 
-# only proceed below if both databases connected above.
+    # only proceed below if both databases connected above.
+    if success == 2:
+        try:
+            os.stat(BACKUP_PATH)
+            print(f"Backup folder ({BACKUP_PATH}) exists!")
+        except:
+            os.mkdir(BACKUP_PATH)
+            print(f"Created backup directory ({BACKUP_PATH})!")
 
-    print("Backing up database...")
-    print("\n")
-    if version == 0:
-        print("Backing up M0 database...")
         print("\n")
-        print("M0 database backed up successfully!")
-    elif version == 1:
-        print("Backing up M1 database...")
+        print(f"Backing up M{version} databases into {BACKUP_PATH}")
         print("\n")
-        print("M1 database backed up successfully!")
+        DATETIME = time.strftime('%Y%m%d-%H%M%S')
+        for db_x in [db_mangosd_name, db_characters_name]:
+            BACKUP_FILE = f"{BACKUP_PATH}{db_x}-{DATETIME}.sql"
+            print(f"Backing up {db_x}...")
+            os.system(f"mysqldump -h {mysql_host} -P {mysql_port} -u {mysql_user} -p{mysql_pass} {db_x} > {BACKUP_FILE}")
+            try: 
+                os.stat(f"{BACKUP_FILE}")
+                if (os.path.getsize(f"{BACKUP_FILE}") > 0):
+                    print(f"Backup of {db_x} successful!")
+                else:
+                    print(f"Backup of {db_x} failed!")
+                    os.remove(f"{BACKUP_FILE}")
+            except:
+                print(f"Error backing up {db_x}!")
     else:
         print("\n")
-        print("Invalid version!")
+        print("Unable to connect to one or more databases! Please confirm your credentials and ensure that the user is authorized to access the databases.")
+
+def migrate_db(version):
+    print("Time to migrate the DB!")
 
 print_banner()
 print_menu()
